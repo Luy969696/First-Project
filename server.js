903e4bf48b570dde328d15954cf0002fc9a48223
Luy969696 <dnrdydsla@gmail.com>
2023-01-18 22:50:21 +0900

    1.2 version

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { response, query } = require('express');
 const express = require('express');
 const fs = require('fs');
 const { request } = require('http');
+var bodyParser = require('body-parser')
 const app = express();
 const ejs = require("ejs");
 const mainPage = fs.readFileSync('data.ejs', 'utf8');
@@ -13,27 +14,65 @@ const conn = mysql.createConnection({  // mysql 접속 설정
     database: 'sakila'
 });
 
-
-app.use(express.static(__dirname+"/public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname+"/public"));
 
 app.listen(8080, function(){
     console.log('listening on 8080');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/data.ejs")
-});
+// app.get('/', (req, res) => {
+//     var page = ejs.render(mainPage, {
+//     title: "Temporary Title",
+//     });
+//     res.send(page);
+//     // res.sendFile(__dirname + "/data.ejs")
+// });
  
-app.get('/getdata?', (req, res) => {
-    
-    conn.query("SELECT * FROM friends;", function(err, result, fields){
+app.post('/profile', function(req, res)  {
+    console.log(req.body)
+  })
+
+
+app.get('/', (req, res) => {
+    conn.query("SELECT * FROM guest_book;", function(err, result, fields){
         if(err) throw err;
         else{
             var page = ejs.render(mainPage, {
                 title: "Temporary Title",
                 data: result,
             });
+            // console.log(result)
             res.send(page);
+            
         }
     });
+});
+
+app.post("/AddData",(req,res)=>{
+    var comment = req.body.Write_comment;
+
+    // var body = req.body;
+    // console.log();
+    conn.query("insert into guest_book (name,context) values ('test',?);", [
+        comment
+      ], function() {
+        res.redirect('/')
+      })
+      
+    // conn.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("1 record inserted");
+    // });
+
+    // conn.query("SELECT * FROM friends;", function(err, result, fields){
+    //     if(err) throw err;
+    //     else{
+    //         var page = ejs.render(mainPage, {
+    //             title: "Temporary Title",
+    //             data: result,
+    //         });
+    //         res.send(page);
+    //     }
+    // });
 });
